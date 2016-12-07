3fc3ce17bff559484e97908e4ecab129f59c8970
galanisd <galanisd@gmail.com>
2016-12-07 18:12:34 +0200

    AppConfig

---
+++ src/main/java/eu/openminted/storage/config/Storage.java
@@ -0,0 +1,10 @@
+package eu.openminted.storage.config;
+
+/**
+ * @author galanisd
+ *
+ */
+public class Storage {
+	public final static String PITHOS = "PITHOS";
+	public final static String LOCAL = "LOCAL";
+}

57e006fe1547ee74fd9ec6a3630d4dad260e8aa1
galanisd <galanisd@gmail.com>
2016-12-12 18:04:59 +0200

    Added more rest services.
    Former-commit-id: a88e12c6cfb0cbabea02bdb77e11cd85d2e27bb8

--- src/main/java/eu/openminted/store/controller/Starter.java
+++ src/main/java/eu/openminted/store/controller/Starter.java
@@ -4,6 +4,11 @@ import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 
+/**
+ * Starts the application. 
+ * @author galanisd
+ *
+ */
 public class Starter {
 	
 	public final static String applicationProperties = "storeApplicationCfg";
@@ -11,8 +16,10 @@ public class Starter {
 	public final static String defaultApplicationProperties = "myApp.properties";
 	
 	public static void main(String[] args) {
-				
+		
+		// Disable restarts.
 		System.setProperty("spring.devtools.restart.enabled", "false");
+		
 		// If required load default applications properties.
 		String appPropFile = System.getProperty(applicationProperties);
 		if(appPropFile == null){
@@ -20,16 +27,16 @@ public class Starter {
 			Properties props = new Properties();						
 			try{								
 				props.load(ApplicationBoot.class.getResourceAsStream(defaultApplicationProperties));
-				System.out.println("==Loading default config:" + props.getProperty(storagePropertiesFile));
+				System.out.println("==> Loading default config:" + props.getProperty(storagePropertiesFile));
 				System.setProperty(applicationProperties, props.getProperty(storagePropertiesFile));				
 			}catch(Exception e){
 				e.printStackTrace();
 			}								
 		}else{
-			System.out.println("==Config:" + System.getProperty(applicationProperties));
+			System.out.println("==> Config:" + System.getProperty(applicationProperties));
 		}
 		
-		// Run
+		// Run app within a Spring Context.
 		SpringApplication springApplication = new SpringApplication(ApplicationBoot.class);
 		springApplication.run();
 	}
--- src/main/java/eu/openminted/store/controller/StoreController.java
+++ src/main/java/eu/openminted/store/controller/StoreController.java
@@ -2,9 +2,14 @@ package eu.openminted.store.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import eu.openminted.store.StoreException;
 import eu.openminted.store.StoreService;
 
 @Controller
@@ -25,25 +31,48 @@ public class StoreController {
     @Autowired
     public StoreController(StoreService storeService) {
        this.storeService = storeService;
-    }    
+    }   
     
-    @RequestMapping(value="/store/listfiles", method=RequestMethod.GET)
+    @RequestMapping(value="/store/createArchive", method=RequestMethod.GET)
+    @ResponseBody
+    public String createArchive(){    	       	    	
+    	return storeService.createArchive() + "";    
+    }
+
+    
+    @RequestMapping(value="/store/listFiles", method=RequestMethod.GET)
     @ResponseBody
     public String listFiles(){    	       	    	
     	return storeService.listAllFiles();     
     }
     
-    @RequestMapping(value="/store/deleteall", method=RequestMethod.GET)
+    @RequestMapping(value="/store/deleteAll", method=RequestMethod.GET)
     @ResponseBody
     public String deleteAll(){    	       	    	
     	return storeService.deleteAll() + "";    
     }
     
-    /*
-    @RequestMapping(value="/", method=RequestMethod.GET)
-    public String listUploadedFiles(Model model) throws IOException {        
-        return "uploadForm";
+    @RequestMapping(value="/storage/downloadFile", method=RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@RequestParam("fileName") String fileName){    	
+       	    	
+        InputStream is = storeService.downloadFile(fileName);
+        Resource resource  = null;
+        
+        try {            
+            resource = new InputStreamResource(is);
+            if(!resource.exists() || !resource.isReadable()) {
+            	throw new StoreException("Could not read file: " + fileName);
+            }
+            
+        } catch (StoreException e) {
+            throw new StoreException("Could not read file: " + fileName, e);
+        }
+            
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(resource);    
+            	
     }
-    */
-
 }

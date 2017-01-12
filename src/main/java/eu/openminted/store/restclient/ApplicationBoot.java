c026061fb05f553d52fd57c14e5179bb6faedd9c
galanisd <galanisd@gmail.com>
2017-01-12 16:22:00 +0200

    Reformated.

--- src/main/java/eu/openminted/store/restclient/ApplicationBoot.java
+++ src/main/java/eu/openminted/store/restclient/ApplicationBoot.java
@@ -13,27 +13,20 @@ public class ApplicationBoot {
 	
 	private static final Logger log = LoggerFactory.getLogger(ApplicationBoot.class);
 	
-	public static void main(String args[]){
-		
+	public static void main(String args[]){		
 		StoreRESTClient store = new StoreRESTClient("http://localhost:8080/");
 		
-		//String result = client.deleteAll();
-		//log.info("Delete all" + result);
-		
+		String result = store.deleteAll();
+		log.info("Delete all" + result);		
 		String archiveID = store.createArchive();
-		log.info("Create Archive:" + archiveID);
-						
+		log.info("Create Archive:" + archiveID);		
 		String subArchiveId = store.createSubArchive(archiveID, "authors");
-		
-		//File f = new File("C:/Users/galanisd/Desktop/Data/_AppTestData/DataForUpload/BILI_TMX_886_el-en.txt.zip");
-		File f = new File("C:/Users/galanisd/Desktop/Data/OpenAIRE/openairemetadata/stelios_metadata/authors.json");
-		//File f = new File("C:/Users/galanisd/Desktop/Data/OpenAIRE/openairemetadata/stelios_metadata/authorspubs.json");
-		
+		log.info("Create SubArchive:" + subArchiveId);		
+		File f = new File("C:/Users/galanisd/Desktop/Data/OpenAIRE/openairemetadata/stelios_metadata/authors.json");		
 		store.updload(f, subArchiveId, f.getName());
-				
+		log.info("Upload to SubArchive:" + subArchiveId);			
 		log.info("List Files");
-		log.info(store.listFiles());
-		
+		log.info(store.listFiles());		
 		log.info("Download");
 		//store.download(archiveID + "/" + f.getName(), "C:/Users/galanisd/Desktop/Data/_AppTestData/Downloaded/" + f.getName());
 		

a5141266e895af60fc3c26f7af502e63b779a46a
galanisd <galanisd@gmail.com>
2017-01-16 12:53:56 +0200

    Added files.

---
+++ src/main/java/eu/openminted/store/config/ApplicationConfigTestParams.java
@@ -0,0 +1,18 @@
+package eu.openminted.store.config;
+
+public class ApplicationConfigTestParams {
+
+	/** Test files location property file */
+	public final static String testFilesCfg = "testFilesCfg";
+		
+	/** Test files locations */
+	public final static String testFilesDefault = "/eu/openminted/store/config/testFiles.properties";
+	
+	// == === ==
+	
+	public final static String bigFile = "bigFile";
+	public final static String downloadFolder = "downloadFolder";
+	
+	public final static String folderWithManyFiles = "folderWithManyFiles";
+	public final static String numOfFileToBeUploaded = "numOfFileToBeUploaded";
+}

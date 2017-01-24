d8a1f6368fd6e2b98850e5c40007f657bf9f624c
galanisd <galanisd@gmail.com>
2017-01-24 18:28:49 +0200

    * Reformatted. * Added more in the commanf line interface.

--- src/main/java/eu/openminted/store/restclient/cmd/ApplicationBoot.java
+++ src/main/java/eu/openminted/store/restclient/cmd/ApplicationBoot.java
@@ -1,5 +1,6 @@
 package eu.openminted.store.restclient.cmd;
 
+import java.io.File;
 import java.util.Scanner;
 
 import org.slf4j.Logger;
@@ -24,96 +25,127 @@ public class ApplicationBoot implements CommandLineRunner {
 	private StoreRESTClient store;
 	private String endpoint;
 	public final static String defaultEndpoint = "http://localhost:8080/";
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("\n\nStarting Store Command Line Client ");		
+		System.out.println("\n\nStarting Store Command Line Client ");
 		setEndpoint(defaultEndpoint);
 		System.out.println("Using default endpoint: " + defaultEndpoint + "\n\n");
-			
-		printHelp();
-		
-        final Scanner console = new Scanner(System.in);
 
-        while (true) {
-        	System.out.print("==> ");
-        	
-        	try{
-	            final String command = console.nextLine();
-	            if (command.equals("quit")) {
-	            	System.out.println("Bye!");
-	                break;
-	            } else if (command.startsWith("setEndpoint")) {
-	            	final String[] allCMDArgs = command.split(" ");
-	            	if(allCMDArgs.length != 2 ){            		
-	            		this.printHelp();
-	            	}else{
-	            		setEndpoint(allCMDArgs[1]);
-	            	}            	
-	            } else if (command.startsWith("printEndpoint")) {
-	            	this.printEndpoint();
-	            }  else if (command.startsWith("createArchive")) {
-	            	System.out.println(store.createArchive());
-	            }  else if (command.startsWith("listFiles")) {
-	            	System.out.println(store.listFiles());
-	            }	else if (command.startsWith("deleteAll")) {
-	            	System.out.println(store.deleteAll());
-	            }
-	        	else if (command.startsWith("finalizeArchive")) {
-	            	final String[] allCMDArgs = command.split(" ");
-	            	if(allCMDArgs.length != 2 ){            		
-	            		this.printHelp();
-	            	}else{
-	            		String archiveID = allCMDArgs[1];
-	            		System.out.println(store.finalizeArchive(archiveID));
-	            	} 	            	
-	            }	            
-	            else if (command.startsWith("help")) {
-	            	this.printHelp();
-	            } else{
-	            	unknownCommand();
-	            }
-        	}catch(Exception e){
-        		error();
-        		printHelp();
-        	}
-        }
+		printHelp();
+
+		final Scanner console = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("StoreClient\\> ");
+
+			try {
+				final String command = console.nextLine();
+				if (command.equals("quit")) {
+					System.out.println("Bye!");
+					break;
+				} else if (command.startsWith("setEndpoint")) {
+					final String[] allCMDArgs = command.split(" ");
+					if (allCMDArgs.length != 2) {
+						check();
+						this.printHelp();
+					} else {
+						setEndpoint(allCMDArgs[1]);
+					}
+				} else if (command.equals("printEndpoint")) {
+					this.printEndpoint();
+				} else if (command.equals("createArchive")) {
+					System.out.println(store.createArchive());
+				} else if (command.equals("listFiles")) {
+					System.out.println(store.listFiles());
+				} else if (command.startsWith("uploadFileToArchive")) {
+					final String[] allCMDArgs = command.split(" ");
+					if (allCMDArgs.length != 3) {
+						check();
+						this.printHelp();
+					} else {
+						String fileName = allCMDArgs[1];
+						String archiveID = allCMDArgs[2];
+						File file = new File(fileName);
+
+						System.out.println(store.updload(file, archiveID, file.getName()));
+					}
+				} else if (command.startsWith("downloadArchive")) {
+					final String[] allCMDArgs = command.split(" ");
+					if (allCMDArgs.length != 3) {
+						check();
+						this.printHelp();
+					} else {
+						String archiveID = allCMDArgs[1];
+						String destination = allCMDArgs[2];
+						System.out.println(store.downloadArchive(archiveID, destination));
+					}
+				} else if (command.equals("deleteAll")) {
+					System.out.println(store.deleteAll());
+				} else if (command.startsWith("finalizeArchive")) {
+					final String[] allCMDArgs = command.split(" ");
+					if (allCMDArgs.length != 2) {
+						check();
+						this.printHelp();
+					} else {
+						String archiveID = allCMDArgs[1];
+						System.out.println(store.finalizeArchive(archiveID));
+					}
+				} else if (command.equals("help")) {
+					this.printHelp();
+				} else {
+					this.unknownCommand();
+					this.printHelp();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				error();
+				printHelp();
+			}
+		}
 	}
-	
-	private void setEndpoint(String enpoint){
+
+	private void setEndpoint(String enpoint) {
 		this.endpoint = enpoint;
 		buildRESTClient();
 	}
-	
-	private void printEndpoint(){
+
+	private void printEndpoint() {
 		System.out.println("endpoint: " + endpoint);
 	}
-	
-    private void unknownCommand() {
-    	System.out.println("Unknown command!");
-    }
-    
-    private void error() {
-    	System.out.println("ERR0R!");
-    }
-    
-    private void printHelp() {
-    	System.out.println("Available commands:");
-        final String format = "%-25s%s%n";
-        // == ===
-        System.out.printf(format, "help", " => Displays this help text");
-        System.out.printf(format, "quit", " => Quit Store Client");
-        // == ===
-        System.out.printf(format, "setEndpoint [endpoint] ", " => Set store endpoint ");
-        System.out.printf(format, "printEndpoint ", " => Prints endpoint");
-        // == ===                
-        System.out.printf(format, "createArchive ", " => Creates archive and returns its id");
-        System.out.printf(format, "listFiles ", " => List all store files");
-        System.out.printf(format, "deleteAll ", " => Delete all store files");
-        System.out.printf(format, "finalizeArchive [archiveID]", " => Finalize Archive");
-               
-    }
-    
+
+	private void check() {
+		System.out.println("Check the syntax of the command...");
+	}
+
+	private void unknownCommand() {
+		System.out.println("Unknown command!");
+	}
+
+	private void error() {
+		System.out.println("ERR0R!");
+	}
+
+	private void printHelp() {
+		System.out.println("Available commands:");
+		final String format = "%-25s%s%n";
+		// == ===
+		System.out.printf(format, "help", " => Displays help.");
+		System.out.printf(format, "quit", " => Quit Store Client.");
+		// == ===
+		System.out.printf(format, "setEndpoint [endpoint] ", " => Set store endpoint. ");
+		System.out.printf(format, "printEndpoint ", " => Prints endpoint.");
+		// == ===
+		System.out.printf(format, "createArchive ", " => Creates archive and returns its id.");
+		System.out.printf(format, "listFiles ", " => List all files in the store.");
+		System.out.printf(format, "deleteAll ", " => Delete all files in the store.");
+		System.out.printf(format, "uploadFileToArchive [filePath] [archiveID] ", " => Uploads a file to an archive");
+		System.out.printf(format, "downloadArchive [archiveID] [destinationPath]  ",
+				" => Downloads an archive zip to destinationPath");
+		System.out.printf(format, "finalizeArchive [archiveID]", " => Finalize Archive.");
+
+	}
+
 	private void buildRESTClient() {
 		store = new StoreRESTClient(endpoint);
 	}
@@ -121,7 +153,7 @@ public class ApplicationBoot implements CommandLineRunner {
 	// == === ==
 	public static void main(String args[]) {
 		SpringApplication app = new SpringApplication(ApplicationBoot.class);
-        app.setBannerMode(Banner.Mode.OFF);
+		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
 	}
 }

fc587eb9b0eafab6168e78ed8f5bd96ca75582e9
ilsp <galanisd@gmail.com>
2017-02-17 19:24:39 +0200

    Added client scripts
    Former-commit-id: a3db4039a543553466391ae01cd5d0bdab11b53e

---
+++ scripts/LinuxStartOMTDStoreClient.sh
@@ -0,0 +1,1 @@
+java -jar $(pwd)"/../omtd-store-rest-client/target/omtd-store-rest-client-0.0.1-SNAPSHOT-exec.jar" 
---
+++ scripts/WINStartOMTDStoreClient.bat
@@ -0,0 +1,2 @@
+java -jar %CD%/../omtd-store-rest-client/target/omtd-store-rest-client-0.0.1-SNAPSHOT-exec.jar 
+pause;

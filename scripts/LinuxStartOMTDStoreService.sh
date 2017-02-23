83362005b255ceb884b6d73bef857bfc31aee8fe
ilsp <galanisd@gmail.com>
2017-02-23 14:51:39 +0200

    * Added OMTDStoreService.sh * Added header to other .sh scripts.

--- scripts/LinuxStartOMTDStoreService.sh
+++ scripts/LinuxStartOMTDStoreService.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 java -DstoreApplicationCfg="file:"$(pwd)"/"$1 -jar $(pwd)"/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar" 
--- scripts/LinuxStartOMTDStoreServiceWithDefaultConfig.sh
+++ scripts/LinuxStartOMTDStoreServiceWithDefaultConfig.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 java -jar $(pwd)/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar 

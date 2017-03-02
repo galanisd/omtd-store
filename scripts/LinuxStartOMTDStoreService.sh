e199b8169f795d265e10ed679c53ab7ed647acd6
ilsp <galanisd@gmail.com>
2017-03-02 22:45:42 +0200

    * Merged Linux scripts for starting REST server.

--- scripts/LinuxStartOMTDStoreService.sh
+++ scripts/LinuxStartOMTDStoreService.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -DstoreApplicationCfg="file:"$(pwd)"/"$1 -jar $(pwd)"/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar" 
+JARPATH=$(pwd)"/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar"
+
+if [ "$#" -eq  "0" ]
+then
+     echo "Store Service will start with default configuration"
+     java -jar $JARPATH
+else
+     java -DstoreApplicationCfg="file:"$(pwd)"/"$1 -jar $JARPATH
+fi

429d337a3f6754780947dcb449497c23bc2a65a5
galanisd <galanisd@gmail.com>
2017-03-07 14:38:17 +0200

    Merged windows start server scripts.

--- scripts/WINStartOMTDStoreService.bat
+++ scripts/WINStartOMTDStoreService.bat
@@ -1,2 +1,11 @@
-java -DstoreApplicationCfg=file:%CD%/%1 -jar %CD%/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar 
+@echo off
+
+if [%1]==[] goto defaultRun
+REM Start rest server with the provided config file.
+java -DstoreApplicationCfg=file:%CD%/%1 -jar %CD%/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar
+goto :eof
+:defaultRun
+REM Start rest server with default config file.
+java -jar %CD%/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar 
+ 
 pause;
--- scripts/WINStartOMTDStoreServiceWithDefaultConfig.bat
+++
@@ -1,2 +0,0 @@
-java -jar %CD%/../omtd-store-rest/target/omtd-store-rest-0.0.1-SNAPSHOT.jar 
-pause;

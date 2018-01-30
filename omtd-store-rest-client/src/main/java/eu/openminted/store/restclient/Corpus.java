e67564798d4290e500fa95d474b68a71530d77fd
Kostas <spyroukon@gmail.com>
2018-01-30 17:55:20 +0200

    update Corpus class

--- omtd-store-rest-client/src/main/java/eu/openminted/store/restclient/Corpus.java
+++ omtd-store-rest-client/src/main/java/eu/openminted/store/restclient/Corpus.java
@@ -26,8 +26,8 @@ public class Corpus {
     private int subsetSize;
     private String archiveId;
     private List<String> filepaths;
-    private List<Publication> set;
-    private RestTemplate restTemplate;
+    private List<Publication> publications = null;
+//    private RestTemplate restTemplate;
     private String endpoint = "http://localhost:8080/"; // FIXME: move to property file.
 
     public Corpus(String archiveId) {
@@ -42,30 +42,97 @@ public class Corpus {
 
         String[] paths = restTemplate.postForObject(endpoint + StoreREST.listFilesInArch, request, String[].class);
 
-        filepaths = new ArrayList<>(Arrays.asList(paths));
+        this.filepaths = new ArrayList<>(Arrays.asList(paths));
+        this.archiveId = archiveId;
+        this.publications = new ArrayList<>();
+
         analyzeFilepaths();
+//        printPublications(this.publications);
     }
     
 
     private void analyzeFilepaths() {
+        int abstractMask    = 0x0001;
+        int fulltextMask    = 0x0010;
+        int metadataMask    = 0x0100;
+        int annotationsMask = 0x1000;
+
         List<String> files;
         if(filepaths == null) {
             totalPublications = 0;
             return;
         }
-        files = filepaths.stream()
-                .map(Paths::get)
-                .map(Path::getFileName)
-                .map(Path::toString)
-                .map(FilenameUtils::removeExtension)
-                .collect(Collectors.toList());
 
-        Set<String> publicationNames = new HashSet<>(files);
+        List<Path> paths = filepaths.stream()
+                                    .map(Paths::get)
+                                    .collect(Collectors.toList());
 
-        totalPublications = publicationNames.size();
-        System.out.println(files);
-        System.out.println("Total files: " + filepaths.size());
-        System.out.println("Total publications: " + getTotalPublications());
+        // save for each publication the name (String) and a value (int)
+        // which represents the existence/absence of abstract, fulltext, metadata and annotations.
+        HashMap<String, Integer> publicationInfo = new HashMap<>();
+
+        // create a set with publication IDs extracted from the filenames.
+        Set<String> publicationId = new HashSet<>();
+
+        for (Path file : paths) {
+            String filename = file.getFileName().toString();
+            filename = FilenameUtils.removeExtension(filename);
+            publicationId.add(filename);
+            publicationInfo.putIfAbsent(filename, 0x0000);
+            if (file.getParent().getFileName().toString().equals("abstract")) {
+                publicationInfo.replace(filename, publicationInfo.get(filename) + abstractMask);
+            } else if (file.getParent().getFileName().toString().equals("fulltext")) {
+                publicationInfo.replace(filename, publicationInfo.get(filename) + fulltextMask);
+            } else if (file.getParent().getFileName().toString().equals("metadata")) {
+                publicationInfo.replace(filename, publicationInfo.get(filename) + metadataMask);
+            } else if (file.getParent().getFileName().toString().equals("annotations")) {
+                publicationInfo.replace(filename, publicationInfo.get(filename) + annotationsMask);
+            } else {
+                //
+            }
+        }
+
+        // create publication entries
+        Iterator it = publicationId.iterator();
+        while (it.hasNext()) {
+            String id = (String) it.next();
+            int value = publicationInfo.get(id);
+            boolean hasAbstract = ((value & abstractMask)==abstractMask);
+            boolean hasFulltext = ((value & fulltextMask)==fulltextMask);
+            boolean hasMetadata = ((value & metadataMask)==metadataMask);
+            boolean hasAnnotations = ((value & annotationsMask)==annotationsMask);
+
+            publications.add(new Publication(id, id/*title here*/, this.archiveId,
+                    hasFulltext, hasAbstract, hasMetadata, hasAnnotations));
+        }
+
+        setTotalPublications(publicationInfo.size());
+    }
+
+    public void printPublications(List<Publication> publications) {
+        if (publications != null) {
+            System.out.println("Printing Publication Info:");
+            for (Publication pub : publications) {
+                System.out.println("\n");
+                System.out.println("Archive ID:\t" + pub.getArchiveId());
+                System.out.println("Pub ID:  \t" + pub.getId());
+                System.out.println("Title:   \t" + pub.getTitle());
+                System.out.println("Abstract:\t" + pub.isHasAbstract());
+                System.out.println("Fulltext:\t" + pub.isHasFulltext());
+                System.out.println("Metadata:\t" + pub.isHasMetadata());
+                System.out.println("Annotations:\t" + pub.isHasAnnotations());
+            }
+        }
+    }
+
+
+    public List<Publication> getCorpusSubset(int from, int size) {
+        if (publications != null)
+            if (publications.size() >= from+size)
+                return this.getPublications().subList(from, (from+size));
+            else if (publications.size() > from)
+                return this.getPublications().subList(from, publications.size());
+        return null;
     }
 
 
@@ -86,28 +153,13 @@ public class Corpus {
         this.totalPublications = totalPublications;
     }
 
-    public int getIndexFrom() {
-        return indexFrom;
+
+    public List<Publication> getPublications() {
+        return publications;
     }
 
-    protected void setIndexFrom(int indexFrom) {
-        this.indexFrom = indexFrom;
-    }
-
-    public int getSubsetSize() {
-        return subsetSize;
-    }
-
-    protected void setSubsetSize(int subsetSize) {
-        this.subsetSize = subsetSize;
-    }
-
-    public List<Publication> getSet() {
-        return set;
-    }
-
-    protected void setSet(List<Publication> set) {
-        this.set = set;
+    protected void setPublications(List<Publication> publications) {
+        this.publications = publications;
     }
 
 }
